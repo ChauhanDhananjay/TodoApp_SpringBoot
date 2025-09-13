19463f96eeb2ec6b349b5c16a00b8a7b30631d60
Dhananjay <dhananjaychauhan965@gmail.com>
2025-09-13 15:24:20 +0530

    added find and delete methods

--- src/main/java/com/gdl/spring/todoapp/ToDoController.java
+++ src/main/java/com/gdl/spring/todoapp/ToDoController.java
@@ -3,10 +3,7 @@ package com.gdl.spring.todoapp;
 import com.gdl.spring.todoapp.Pojo.TodoList;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,5 +24,25 @@ public class ToDoController {
     public ResponseEntity<TodoList> createTodo(@RequestBody TodoList newToDo){
         todoLists.add(newToDo);
         return ResponseEntity.status(HttpStatus.CREATED).body(newToDo);
+    }
+    @GetMapping("/todoList/{todoId}")
+    public ResponseEntity<?> findTodoById(@PathVariable int todoId){
+        for (TodoList l1:todoLists){
+            if (l1.getId()==todoId){
+                return ResponseEntity.status(HttpStatus.FOUND).body(l1);
+            }
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Id not found");
+    }
+    @DeleteMapping("/todoList/{deleteId}")
+    public ResponseEntity<?> deleteTodo(@PathVariable int deleteId){
+        for (TodoList l1:todoLists){
+            if (l1.getId()==deleteId){
+               return ResponseEntity.status(HttpStatus.OK).body("Deleted");
+            }
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Not Deleted. Id not found");
     }
 }
